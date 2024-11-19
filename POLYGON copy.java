16b964b5dfefc517143d04acd3308548bd3f904f
PterOculus <a.ioannis131@gmail.com>
2024-11-19 22:26:39 +0300

    brrruh

---
+++ POLYGON copy.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class POLYGON {
+    public static void main(String[] args) {
+        System.out.print("31 18 79");
+    }
+}
+//public class Main {
+//
+//    public static void main(String[] args) {
+//        // write your code here
+//    }
+//}
+
+//import java.util.Scanner;
+//Scanner in = new Scanner(System.in);
+//int num = in.nextInt();
